de8267bf2f9a791a914b736ecef9a31b91086a05
Ricardo Hernandez <ricahernandez0001@gmail.com>
2024-08-01 08:19:00 -0400

    Day 9

---
+++ Day 009 - Python Dictionaries/main.py
@@ -0,0 +1,33 @@
+from replit import clear
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+
+bidders = {}
+stillBidding = True
+
+
+def add_bidder(name, bid):
+  bidders[name] = bid
+
+def check_winner():
+  higest_bid = 0
+  winner = ""
+  for bidder in bidders:
+    if bidders[bidder] > higest_bid:
+      higest_bid = bidders[bidder]
+      winner = bidder
+  return winner
+
+print(logo)
+print("Welcome to the secret auction program. \n")
+
+while stillBidding:
+  name = input("What is your name?:\n")
+  bid = int(input("What's your bid? $\n"))
+  add_bidder(name, bid)
+  continueBidding = input("Are there any other bidders? Type 'yes' or 'no'.")
+  if continueBidding == 'no':
+    stillBidding = False
+    print(check_winner())
+  else:
+    clear()
---
+++ Day 009 - Python Dictionaries/notes.md
@@ -0,0 +1,10 @@
+# Day 9 of 100 Days of Code
+
+## Notes
+
+### Python Dictionaries and Nested Dictionaries
+
+- Day 9 focused mainly on dictionaries and nested dictionaries in Python.
+- It reminded me a lot of Java HashMaps.
+- This left me wondering about the performance differences between Python and Java, particularly in terms of data usage with nesting lists and dictionaries, and the efficiency of Python functions.
+- Overall, I had very little trouble understanding the material and could complete the day's exercises in under an hour.
