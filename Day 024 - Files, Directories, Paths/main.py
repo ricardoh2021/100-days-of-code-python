3760ef38dd1b110078820960f9467b940b980d7c
Ricardo Hernandez <ricahernandez0001@gmail.com>
2024-08-22 13:50:59 -0400

    Day 24 Code

---
+++ Day 024 - Files, Directories, Paths/main.py
@@ -0,0 +1,8 @@
+# with open("my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+import os
+
+with open("/Users/ricardohernandez/desktop/new_file.txt", mode="w") as file:
+    file.write("New text Test.")
