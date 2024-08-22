f99e57a8ab9ceb5c260a09db43961496a707efc7
Ricardo Hernandez <ricahernandez0001@gmail.com>
2024-08-22 14:07:52 -0400

    Day 24 Mail Merge Project

--- Day 024 - Files, Directories, Paths/Mail Merge Project Start/main.py
+++ Day 024 - Files, Directories, Paths/Mail Merge Project Start/main.py
@@ -1,4 +1,25 @@
-#TODO: Create a letter using starting_letter.txt 
+#TODO: Create a letter using starting_letter.txt
+with open("./Input/Letters/starting_letter.txt") as file:
+    starting_letter = file.read()
+
+print(starting_letter)
+
+names = []
+with open("./Input/Names/invited_names.txt") as name_file:
+        for line in name_file:
+            name = line.rstrip()
+            names.append(name)
+
+print(names)
+
+# Replace the target string
+for name in names:
+    personal_letter = starting_letter.replace('[name]', name)
+    with open(f"./Output/ReadyToSend/letter_for_{name}.docx", 'w') as file:
+        file.write(personal_letter)
+
+# Write the file out again
+
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
